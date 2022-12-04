85eb2badac52ba2382d13ab823978a99ef8ab499
LeoCosta <leocosta.black@hotmail.com>
2022-12-04 14:28:24 -0300

    Create input validation with FormState

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,12 +15,15 @@ class _HomeState extends State<Home> {
   TextEditingController heightInputController = TextEditingController();
   IMCCheck? _imcResult;
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   // Functions
   void _resetFields() {
+    weightInputController.text = '';
+    heightInputController.text = '';
     setState(() {
-      weightInputController.text = '';
-      heightInputController.text = '';
       _imcResult = null;
+      _formKey = GlobalKey<FormState>(); // Reset errors message
     });
   }
 
@@ -38,83 +41,100 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculadora de IMC'),
+        title: const Text('Calculadora de IMC'),
         centerTitle: true,
         actions: [
           IconButton(
-            icon: Icon(Icons.refresh),
+            icon: const Icon(Icons.refresh),
             onPressed: _resetFields,
           )
         ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Icon(Icons.person, size: 120),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16),
-              child: TextField(
-                controller: weightInputController,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Icon(Icons.person, size: 120),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: TextFormField(
+                  controller: weightInputController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Peso (kg)',
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: mainColor),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Insira seu peso';
+                    }
+                  },
+                ),
+              ),
+              TextFormField(
+                controller: heightInputController,
                 keyboardType: TextInputType.number,
                 decoration: const InputDecoration(
-                  labelText: 'Peso (kg)',
+                  labelText: 'Altura (cm)',
                   border: OutlineInputBorder(),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: mainColor),
                   ),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Insira sua altura';
+                  }
+                },
               ),
-            ),
-            TextField(
-              controller: heightInputController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Altura (cm)',
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: mainColor),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _calculate();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  child: const Text('Calcular'),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 24),
-              child: ElevatedButton(
-                onPressed: _calculate,
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.infinity, 50),
-                ),
-                child: const Text('Calcular'),
-              ),
-            ),
-            Column(
-              children: [
-                if (_imcResult != null)
-                  const Opacity(
-                    opacity: 0.4,
-                    child: Padding(
-                      padding: EdgeInsets.only(bottom: 4),
-                      child: Text(
-                        'Resultado:',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 16,
+              Column(
+                children: [
+                  if (_imcResult != null)
+                    const Opacity(
+                      opacity: 0.4,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          'Resultado:',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
+                  Text(
+                    _imcResult == null ? 'Informe seus dados...' : _imcResult!.resultMessage,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: _imcResult == null ? mainColor : _imcResult!.color,
+                    ),
                   ),
-                Text(
-                  _imcResult == null ? 'Informe seus dados...' : _imcResult!.resultMessage,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: _imcResult == null ? mainColor : _imcResult!.color,
-                  ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
